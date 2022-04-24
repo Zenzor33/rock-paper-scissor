a7d57bc4e611a7f1349e3759bf6c12a8d6c01348
Zenzor33 <rzeidler@gmail.com>
2022-04-24 18:09:54 -0300

    building

--- javascript.js
+++ javascript.js
@@ -1,7 +1,32 @@
 /* Write a function that plays a single round of Rock Paper Scissors. 
 The function should take two parameters - the playerSelection and 
-computerSelection - and then return a string that declares the winner of 
+return and then return a string that declares the winner of 
 the round like so: "You Lose! Paper beats Rock" */
 
 let playerSelection = window.prompt("Selection: ")
-console.log(playerSelection);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function compSelection() {
+    let compRNG = getRandomInt(3); // random int between 0 & 2
+
+    if (compRNG < 1) {
+        return 'Rock';
+    }
+    if (compRNG < 2) {
+        return 'Paper';
+    }
+    if (compRNG < 3 ) {
+        return 'Scissor';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    console.log(`${playerSelection.toLowerCase()} ${computerSelection.toLowerCase()}`);
+    }
+
+playRound(playerSelection, compSelection());
+
+
