b4466ac2c78d5f973532d7650918bc449471d713
Zenzor33 <rzeidler@gmail.com>
2022-04-24 21:42:04 -0300

    added-conditionals

--- javascript.js
+++ javascript.js
@@ -10,21 +10,48 @@ function getRandomInt(max) {
   }
 
 function compSelection() {
-    let compRNG = getRandomInt(3); // random int between 0 & 2
+    const compRNG = getRandomInt(3); // random int between 0 & 2
 
-    if (compRNG < 1) {
+    if (compRNG == 0) {
         return 'Rock';
     }
-    if (compRNG < 2) {
+    if (compRNG == 1) {
         return 'Paper';
     }
-    if (compRNG < 3 ) {
+    if (compRNG == 2 ) {
         return 'Scissor';
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log(`${playerSelection.toLowerCase()} ${computerSelection.toLowerCase()}`);
+    console.log(`you play: ${playerSelection.toLowerCase()} 
+computer plays: ${computerSelection.toLowerCase()}`);
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+    let playerWins = 'you win';
+    let playerLoses = 'you lose';
+    while (true) {
+        if (player == 'rock' && computer == 'scissor') {
+            console.log(playerWins);
+            break;
+        }
+        if (player == 'paper' && computer == 'rock') {
+            console.log(playerWins);
+            break;
+        }
+        if (player == 'scissor' && computer == 'paper') {
+            console.log(playerWins);
+            break;
+        }
+        if (player == computer) {
+            console.log('Tie');
+            break;
+        }
+        else {
+            console.log(playerLoses);
+            break;
+    }
+    }
     }
 
 playRound(playerSelection, compSelection());
